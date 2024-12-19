7ccf91425cd404f2649ce981fffd482c0e6da140
Stradioto <45260065+Stradioto@users.noreply.github.com>
2024-12-19 15:08:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import preact from '@preact/preset-vite'
 
 export default defineConfig({
   plugins: [preact()],
-  base: './', // This ensures assets are loaded correctly
+  base: '/',
   build: {
-    outDir: 'dist', // Specify the build output directory
-    assetsDir: 'assets', // Where to place assets
-    sourcemap: false, // Disable sourcemaps in production
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
   },
   css: {
     postcss: './postcss.config.js',
